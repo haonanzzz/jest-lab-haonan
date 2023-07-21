4eb3fe57d91b1c7dc209b8e79ab0673b3d00d05c
haonanzzz <hz96@uw.edu>
2023-07-20 19:44:45 -0700

    jestlab

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
 
-test('renders learn react link', () => {
+test('Class: checking checkbox function', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  //event: click on the checkbox
+  userEvent.click(screen.getByTestId('checkbox'));
+ // checking if checkbox is not checked.
+  expect(screen.getByTestId('checkbox')).toBeInChecked();
 });
+
+test('Individual: checking text information', () => {
+  render(<App />);
+
+  screen.debug();
+
+  expect(screen.getByText('This checkbox is now set to false')).toBeInTheDocument();
+});
+
+
+
